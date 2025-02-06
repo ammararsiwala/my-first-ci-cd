eade4ef4f2d6d8781b2640f40133b3b7ee4eced0
ammararsiwala <AmmarArsiwala@Eaton.com>
2025-02-06 16:12:53 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,11 +1,16 @@
 import hello
 
+
 def test_output(capfd):
     # Run the script
     hello.main()
-    
+
     # Capture the output
     out, err = capfd.readouterr()
-    
+
     # Check the output
-    assert out.strip() == "Hello World\nI MADE MY FIRST CHANGE\nNOW LET'S SEE IF I GET AN ERROR OR NO"
+    assert out.strip() == (
+        "Hello World\n"
+        "I MADE MY FIRST CHANGE\n"
+        "NOW LET'S SEE IF I GET AN ERROR OR NO"
+    )
